cbc1b3cd117bb9b38f9e08509627170d4cc1b8e3
bosung <qweasdzxc0311@naver.com>
2026-02-19 21:58:18 +0900

    refactor: formData Store에 구독 알림 기능 추가

--- store/formData.js
+++ store/formData.js
@@ -8,6 +8,23 @@ const formData = {
     isValid: false,
     isEdit: false,
     dailyId: null,
+    listeners: {},
+
+    subscribe(key, listener) {
+        if (!this.listeners[key]) this.listeners[key] = new Set();
+        this.listeners[key].add(listener);
+        return () => this.listeners[key].delete(listener);
+    },
+
+    notify(key) {
+        if (this.listeners[key]) {
+            this.listeners[key].forEach((fn) => fn(this[key], this));
+        }
+    },
+
+    notifyAll() {
+        Object.keys(this.listeners).forEach((key) => this.notify(key));
+    },
 
     init() {
         this.date = new Date().toISOString().split('T')[0];
@@ -19,6 +36,7 @@ const formData = {
         this.checkAndNotify();
         this.isEdit = false;
         this.dailyId = null;
+        this.notifyAll();
     },
 
     isValidListeners: new Set(),
@@ -31,37 +49,46 @@ const formData = {
         this.payment = payment;
         this.category = category;
         this.sign = sign;
+        this.notifyAll();
     },
 
     setSign(value) {
         this.sign = value;
+        this.notify('amount');
     },
 
     setDate(dateValue) {
         this.date = dateValue;
         this.checkAndNotify();
+        this.notify('date');
     },
     setAmount(amountValue) {
-        this.amount = Number(amountValue.replaceAll(',', ''));
+        this.amount = amountValue;
         this.checkAndNotify();
+        this.notify('amount');
     },
     setDescription(descriptionValue) {
         this.description = descriptionValue;
         this.checkAndNotify();
+        this.notify('description');
     },
     setPayment(paymentValue) {
         this.payment = paymentValue;
         this.checkAndNotify();
+        this.notify('payment');
     },
     setCategory(categoryValue) {
         this.category = categoryValue;
         this.checkAndNotify();
+        this.notify('category');
     },
     setEdit(editValue) {
         this.isEdit = editValue;
+        this.notify('isEdit');
     },
     setDailyId(idValue) {
         this.dailyId = idValue;
+        this.notify('dailyId');
     },
 
     checkValid() {
@@ -71,7 +98,7 @@ const formData = {
 
     subscribeIsValid(listener) {
         this.isValidListeners.add(listener);
-        return () => this._isValidListeners.delete(listener);
+        return () => this.isValidListeners.delete(listener);
     },
 
     checkAndNotify() {
