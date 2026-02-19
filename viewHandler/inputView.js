ffbaaf3720923c54598ac9f520f0b176d19494a0
bosung <qweasdzxc0311@naver.com>
2026-02-19 21:59:08 +0900

    refactor: inputViewHandler input마다 변경되도록 최적화

--- viewHandler/inputView.js
+++ viewHandler/inputView.js
@@ -1,31 +1,41 @@
 import { formatAmount } from '../utils.js';
 
-export function bindInputValue(formData) {
-    const { amount, date, description, category, payment } = formData;
-
+function renderAmountInput(amount, sign) {
+    const $input = document.getElementById('valueInput');
     const $signImg = document.getElementById('sign');
 
-    if (amount > 0) {
-        $signImg.setAttribute('src', 'public/plus.svg');
-    } else {
-        $signImg.setAttribute('src', 'public/minus.svg');
-    }
-
-    const inputItems = {
-        value: formatAmount(Math.abs(amount)) != '0' || '',
-        date,
-        description,
-    };
-
-    Object.entries(inputItems).forEach(([key]) => {
-        const $input = document.querySelector(`#${key}Input`);
-        if ($input) {
-            $input.value = inputItems[key] ?? '';
-        }
-    });
-    const $category = document.querySelector('#dropdown-toggle-category');
-    const $payment = document.querySelector('#dropdown-toggle-payment');
-
-    $category.textContent = category || '선택하세요.';
-    $payment.textContent = payment || '선택하세요.';
+    if ($input) $input.value = formatAmount(Math.abs(amount)) || '';
+    if ($signImg)
+        $signImg.setAttribute(
+            'src',
+            sign ? '/public/minus.svg' : '/public/plus.svg',
+        );
 }
+
+function renderDateInput(date) {
+    const $input = document.getElementById('dateInput');
+    if ($input) $input.value = date || '';
+}
+
+function renderCategoryInput(category) {
+    const $category = document.getElementById('dropdown-toggle-category');
+    if ($category) $category.textContent = category || '선택하세요.';
+}
+
+function renderPaymentInput(payment) {
+    const $payment = document.getElementById('dropdown-toggle-payment');
+    if ($payment) $payment.textContent = payment || '선택하세요.';
+}
+
+function renderDescriptionInput(description) {
+    const $input = document.getElementById('descriptionInput');
+    if ($input) $input.value = description || '';
+}
+
+export {
+    renderAmountInput,
+    renderCategoryInput,
+    renderDateInput,
+    renderDescriptionInput,
+    renderPaymentInput,
+};
