a38753c12dd47e777293b9690ac6cc7c5c1b5658
bosung <qweasdzxc0311@naver.com>
2026-02-20 23:38:20 +0900

    refactor: 이벤트 바인딩 위치 변경 및  form에 선택되어있는 아이디가 있는지 상태로 판단

--- components/dailyList/index.js
+++ components/dailyList/index.js
@@ -1,8 +1,51 @@
+import { dailyData } from '../../store/daily.js';
+import formData from '../../store/formData.js';
 import { renderDailyView } from '../../viewHandler/dailyView.js';
 
 export default function initalizeDailyList() {
     const today = new Date().toISOString().split('T')[0];
     const [year, month] = today.split('-');
-
+    const $dailyRoot = document.querySelector('#daily-placeholder');
+    bindEvents($dailyRoot);
     renderDailyView(year, month);
+}
+
+function bindEvents($rootElement) {
+    $rootElement.addEventListener('click', (e) => {
+        // 필터 상태 변경 로직
+        if (e.target.closest('#filter-income')) {
+            dailyData.toggleIncomeFilter();
+            return;
+        }
+
+        if (e.target.closest('#filter-expense')) {
+            dailyData.toggleExpenseFilter();
+            return;
+        }
+
+        // 삭제 버튼 클릭시 삭제
+        const $deleteBtn = e.target.closest('.daily-delete-btn');
+        if ($deleteBtn) {
+            dailyData.removeDailyData(selectedId);
+            return;
+        }
+
+        // 라인  클릭 로직
+        const $dailyLine = e.target.closest('.daily-line');
+        if (!$dailyLine) return;
+
+        const selectedId = $dailyLine.getAttribute('id');
+        if (formData.dailyId == selectedId) {
+            formData.init();
+            return;
+        }
+
+        formData.setEdit(true);
+        formData.setDailyId(selectedId);
+
+        const { date, items } = dailyData.findDailyDataById(selectedId);
+        items.amount > 0 ? (items.sign = true) : (items.sign = false);
+
+        formData.setFormData(date, items);
+    });
 }
--- viewHandler/dailyView.js
+++ viewHandler/dailyView.js
@@ -1,5 +1,3 @@
-import { dailyData } from '../store/daily.js';
-import formData from '../store/formData.js';
 import createDailyList from '../components/dailyList/dailyList.js';
 
 export function renderDailyView(year, month) {
@@ -8,50 +6,4 @@ export function renderDailyView(year, month) {
     $dailyRoot.innerHTML = '';
 
     $dailyRoot.append(createDailyList(year, month));
-
-    addFilterEventListener('filter-income', $dailyRoot, () =>
-        dailyData.toggleIncomeFilter(),
-    );
-    addFilterEventListener('filter-expense', $dailyRoot, () =>
-        dailyData.toggleExpenseFilter(),
-    );
-
-    bindListClickEvent($dailyRoot);
-}
-
-function addFilterEventListener(targetId, $rootElement, filterFn) {
-    $rootElement.querySelector(`#${targetId}`).addEventListener('click', () => {
-        filterFn();
-    });
-}
-
-function bindListClickEvent($rootElement) {
-    $rootElement.addEventListener('click', (e) => {
-        const $dailyLine = e.target.closest('.daily-line');
-
-        if (!$dailyLine) return;
-
-        const $deleteBtn = e.target.closest('.daily-delete-btn');
-        const seletedId = $dailyLine.getAttribute('id');
-
-        if ($deleteBtn) {
-            dailyData.removeDailyData(seletedId);
-            return;
-        }
-
-        if ($dailyLine.classList.contains('selected')) {
-            $dailyLine.classList.remove('selected');
-            formData.init();
-            return;
-        }
-
-        $dailyLine.classList.add('selected');
-        formData.setEdit(true);
-        formData.setDailyId(seletedId);
-
-        const { date, items } = dailyData.findDailyDataById(seletedId);
-        items.amount > 0 ? (items.sign = true) : (items.sign = false);
-
-        formData.setFormData(date, items);
-    });
 }
