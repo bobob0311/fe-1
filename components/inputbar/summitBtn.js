8f0682a253e8fe203db10f94405d3e8cb1d7c5b0
bosung <qweasdzxc0311@naver.com>
2026-03-01 22:19:11 +0900

    refactor: from제출 버튼 뷰 분리

--- components/inputbar/summitBtn.js
+++ components/inputbar/summitBtn.js
@@ -1,48 +1,23 @@
 import { createElement } from '../../utils.js';
-import formData from '../../store/formData.js';
-import { dailyData } from '../../store/daily.js';
-import dateData from '../../store/date.js';
 
-export default function createSummitButton() {
-    const summitBtnInnerHtml = `
-            <button class="add-button" disabled>
-                <img
-                    src="/public/check.svg"
-                    width="24px"
-                    height="24px"
-                />
-            </button>
-        `;
+export default function createSummitFormButton(isValid) {
+    const $btnImg = createElement('img', {
+        src: '/public/check.svg',
+        width: '24px',
+        height: '24px',
+    });
 
-    const $summitBtn = createElement(
-        'div',
+    const $summitButton = createElement(
+        'button',
         {
-            class: 'add-button-wrappe',
+            class: `add-button`,
+            disabled: !isValid,
         },
-        summitBtnInnerHtml,
+        $btnImg,
     );
-
-    const $btn = $summitBtn.firstElementChild;
-    $btn.addEventListener('click', () => {
-        if (!formData.isValid) return;
-        if (formData.isEdit) {
-            dailyData.changeDailyData(formData);
-        } else {
-            dailyData.uploadDailyData(formData);
-        }
-
-        const { year: nowYear, month: nowMonth } = dateData;
-        const [year, month] = formData.date.split('-');
-        if (nowYear == year && month == nowMonth) formData.init();
-    });
-
-    formData.subscribeIsValid((isValid) => {
-        if (isValid) {
-            $btn.removeAttribute('disabled');
-        } else {
-            $btn.disabled = true;
-        }
-    });
-
-    return $summitBtn;
+    return createElement(
+        'div',
+        { class: 'add-button-wrapper', 'data-action': 'submit-daily' },
+        $summitButton,
+    );
 }
