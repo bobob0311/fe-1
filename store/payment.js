73f8b7b641ef5a2c498ea576ceec1344edd51766
bosung <qweasdzxc0311@naver.com>
2026-03-01 22:24:39 +0900

    feat: payment 구독 시스템 추가 및 modal 변경으로 deletePayment modalState에 따라 적용되도록 변경

--- store/payment.js
+++ store/payment.js
@@ -1,10 +1,26 @@
 const payment = {
     data: ['현금', '신용카드'],
+
+    listeners: {},
+
+    subscribe(key, callback) {
+        if (!this.listeners[key]) {
+            this.listeners[key] = [];
+        }
+        this.listeners[key].push(callback);
+    },
+
+    notify(key) {
+        this.listeners[key]?.forEach((cb) => cb());
+    },
+
     addPayment(value) {
         this.data.push(value);
+        this.notify('payment');
     },
-    filterByValue(value) {
-        this.data = this.data.filter((item) => item !== value);
+    deletePayment(modalState) {
+        this.data = this.data.filter((item) => item !== modalState.value);
+        this.notify('payment');
         return this.data;
     },
 };
