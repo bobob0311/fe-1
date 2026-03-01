390345c00bdfd3c8817c209dedc1fc585e8e4ae1
bosung <qweasdzxc0311@naver.com>
2026-03-01 22:41:01 +0900

    feat: input과 FormData가 일치할 수 있도록 바인딩 함수추가

--- controller/inputController.js
+++ controller/inputController.js
@@ -1,13 +1,20 @@
+import renderModal from '../components/modal/index.js';
 import formData from '../store/formData.js';
+import payment from '../store/payment.js';
 import {
     renderAmountInput,
     renderCategoryInput,
     renderDateInput,
     renderDescriptionInput,
     renderPaymentInput,
+    renderSummitBtn,
 } from '../viewHandler/inputView.js';
 
+// sign을 하나 따로 파야할지도 왜냐면 category가 sign에 따라 바뀌어야해서 초기화가 필요합니다.
 export default function bindFormDataToInputs() {
+    payment.subscribe('payment', renderPayment);
+    formData.subscribe('payment', renderPayment);
+
     formData.subscribe('amount', () =>
         renderAmountInput(formData.amount, formData.sign),
     );
@@ -16,7 +23,20 @@ export default function bindFormDataToInputs() {
         renderDescriptionInput(formData.description),
     );
     formData.subscribe('category', () =>
-        renderCategoryInput(formData.category),
+        renderCategoryInput(
+            formData.category,
+            formData.isCategoryOpen,
+            formData.sign,
+        ),
     );
-    formData.subscribe('payment', () => renderPaymentInput(formData.payment));
+    formData.subscribe('validity', () => {
+        renderSummitBtn(formData.isValid);
+    });
+    formData.subscribe('modal', () => {
+        debugger;
+        renderModal(formData.modal);
+    });
 }
+
+const renderPayment = () =>
+    renderPaymentInput(formData.payment, formData.isPaymentOpen);
