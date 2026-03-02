7543e6c09b6627b87c2562d4f69adac5ca8e4f4f
bosung <qweasdzxc0311@naver.com>
2026-03-02 17:27:41 +0900

    feat: formdate 초기화와 InputBox 셋업 함수 추가

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -1,12 +1,16 @@
 import setupHeader from '../features/header/index.js';
+import setupInputBox from '../features/inputBox/index.js';
 import dateData from '../store/date.js';
+import formData from '../store/formData.js';
+import dailyData from '../store/daily.js';
 
 export default async function bootstrap() {
     // await dailyData.init();
     dateData.init();
+    formData.init();
 
     setupHeader({ dateData });
-    // setupInputBox();
+    setupInputBox({ formData, dailyData });
     // setupDailyList();
 }
 
