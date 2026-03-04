3064a148a15b8c329ccbc19c2f3a40b2401d8be1
bosung <qweasdzxc0311@naver.com>
2026-03-04 23:08:53 +0900

    refactor

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -3,32 +3,14 @@ import setupInputBox from '../features/inputBox/index.js';
 import dateData from '../store/date.js';
 import formData from '../store/formData.js';
 import dailyData from '../store/daily.js';
+import setupDailyList from '../features/daily/index.js';
 
 export default async function bootstrap() {
-    // await dailyData.init();
+    await dailyData.init();
     dateData.init();
     formData.init();
 
     setupHeader({ dateData });
     setupInputBox({ formData, dailyData });
-    // setupDailyList();
+    setupDailyList();
 }
-
-/*
-await dailyData.init();
-
-dailyData.subscribe(() => {
-    renderDailyView(dateData.year, dateData.month);
-});
-
-dateData.subscribe(({ year, month }) => {
-    renderDailyView(year, month);
-    renderDateView(year, month);
-});
-
-document.getElementById('header-root').append(initializeHeader());
-initalizeInputBox();
-initalizeDailyList();
-
-dateData.initDateData();
-*/
